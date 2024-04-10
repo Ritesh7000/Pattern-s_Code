1676cee523041a12e9f93e231095caf6da10db0b
Ritesh Patil <143477849+Ritesh7000@users.noreply.github.com>
2024-04-10 12:08:13 +0530

    Add files via upload

---
+++ Pattern_1.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int n;
+    scanf("%d", &n);
+  	// Complete the code to print the pattern.
+
+
+	return 0;
+}
+
+/*
+Sample Input 0
+
+2
+Sample Output 0
+
+2 2 2
+2 1 2
+2 2 2
+
+Sample Input 1
+5
+Sample Output 1
+
+5 5 5 5 5 5 5 5 5 
+5 4 4 4 4 4 4 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 2 1 2 3 4 5 
+5 4 3 2 2 2 3 4 5 
+5 4 3 3 3 3 3 4 5 
+5 4 4 4 4 4 4 4 5 
+5 5 5 5 5 5 5 5 5
+*/
